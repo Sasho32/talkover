be7dbefa59a819a1a8582e5172eff5d0723efdf4
Sasho Aleksandrov <sashosasho32@gmail.com>
2023-06-26 13:57:15 +0300

    Global user state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 
 function App() {
+    const [user, setUser] = useState('user');
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
+            console.log(`User changing with value of ${user}`);
+            setUser(user);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
     return <div>App</div>;
 }
 
