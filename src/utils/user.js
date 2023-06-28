17f8224f25cabf4ff2da9f71af37c67976242bad
Sasho Aleksandrov <sashosasho32@gmail.com>
2023-06-28 13:14:52 +0300

    sign in, sign up methods

--- src/utils/user.js
+++ src/utils/user.js
@@ -1,8 +1,51 @@
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../firebase';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import {
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+} from 'firebase/auth';
+import { auth, db } from '../firebase';
 
 export async function getUserRecord(userId) {
     const docRef = doc(db, 'users', userId);
     const userDoc = await getDoc(docRef);
     return userDoc.data();
+}
+
+export async function signIn(credentials) {
+    await signInWithEmailAndPassword(
+        auth,
+        credentials.username + '@talkoverdomain.com',
+        credentials.password
+    );
+}
+export async function signUp(credentials) {
+    const { user } = await createUserWithEmailAndPassword(
+        auth,
+        credentials.username + '@talkoverdomain.com',
+        credentials.password
+    );
+
+    console.log(user);
+
+    await setDoc(
+        doc(db, 'users', user.uid),
+        createUserRecord(user.email, user.uid)
+    );
+}
+
+function extractUsername(email) {
+    return email.split('@').at(0);
+}
+
+function createUserRecord(email, id) {
+    return {
+        username: extractUsername(email),
+        uid: id,
+        bio: '',
+        intruder: 'false',
+        posts: 0,
+        comments: [],
+        role: 'user',
+        joinedOn: serverTimestamp(),
+    };
 }
