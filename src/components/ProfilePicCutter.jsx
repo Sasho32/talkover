b028dd47093ceab7dbf70d4b4f452b20423f6820
Sasho Aleksandrov <sashosasho32@gmail.com>
2023-07-22 15:57:57 +0300

    modified the profile picture component after a research - turns out this is the most common practice

--- src/components/ProfilePicCutter.jsx
+++ src/components/ProfilePicCutter.jsx
@@ -1,23 +1,27 @@
-import { useContext } from 'react';
-import { UserContext } from '../UserContext';
 import { useNavigate } from 'react-router-dom';
 import './ProfilePicCutter.scss';
 
-function ProfilePicCutter({ src }) {
-    const { userRecord } = useContext(UserContext);
+function pxToRem(value) {
+    return value / 16;
+}
+
+function ProfilePicCutter({ user, width }) {
     const navigate = useNavigate();
 
-    function goToProfile() {
-        if (userRecord) navigate(`/community/${userRecord.uid}`);
-    }
+    if (!user) return <div className="profile-pic-cutter skeleton"></div>;
+    // за директен достъп от url - на header-а се подава userRecord на profileCutter-а, което първоначално е null
+
+    const style = user.pic ? { backgroundImage: `url(${user.pic})` } : {};
+    // ако е само background пренаписва и другите пропъртита в css файла
 
     return (
-        <div onClick={goToProfile} className="profile-pic-cutter">
-            {src ? (
-                <img src={src} alt="profile_pic" />
-            ) : (
-                userRecord?.username[0].toUpperCase()
-            )}
+        <div
+            style={style}
+            onClick={() => navigate(`/community/${user.uid}`)}
+            className={`profile-pic-cutter ${user.pic ? '' : 'empty'}`}
+        >
+            {!user.pic && user.username[0].toUpperCase()}
+            {/* не е с or, за да не eval-не в url-а */}
         </div>
     );
 }
