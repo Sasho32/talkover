ccc7f360bc17fdc12829b2e55b5333fc667cb7c8
Sasho Aleksandrov <sashosasho32@gmail.com>
2023-08-02 17:55:02 +0300

    now every time a post is fetched, its author is fetched too(if not present in the cache) because i need to display his profile pic and name but by default i only have his id
    views, likes/dislikes functionality added

--- src/pages/home/posts/single-post/SinglePostPage.jsx
+++ src/pages/home/posts/single-post/SinglePostPage.jsx
@@ -1,17 +1,22 @@
-import { useState, useContext } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useState, useContext, Profiler } from 'react';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import { UserContext } from '../../../../contexts/UserContext';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, increment, updateDoc } from 'firebase/firestore';
 import { db } from '../../../../firebase';
 import { calculateRelativeTime } from '../../../../utils/relative-time';
 import DOMPurify from 'dompurify';
 import 'react-quill/dist/quill.snow.css';
+import ProfilePicCutter from '../../../../components/ProfilePicCutter';
 import './SinglePostPage.scss';
+import { useEffect } from 'react';
 
 function SinglePostPage() {
     const [showComments, setShowComments] = useState(false);
     const { userRecord } = useContext(UserContext);
+
+    const [author, setAuthor] = useState(null);
+    const queryClient = useQueryClient();
 
     let { postId } = useParams();
 
@@ -28,12 +33,147 @@ function SinglePostPage() {
 
             if (!post) return null;
 
+            if (post.author !== userRecord.uid) {
+                updateViews();
+            }
+
+            const authorCache = queryClient.getQueryData([
+                'users',
+                post.author,
+            ]);
+
+            if (authorCache) {
+                setAuthor(authorCache);
+            } else {
+                const authorRef = doc(db, 'users', post.author);
+                const authorReceived = await getDoc(authorRef);
+                setAuthor(authorReceived.data());
+            }
+            // ако трябва да го фетчваме -> значи няма кеш -> няма да го ъпдейтваме/сетваме, защото така или иначе искам да фетчва usera при всеки hit на community/:userId
+
             return post;
         },
         {
             refetchOnWindowFocus: false,
+            enabled: !!userRecord,
         }
     );
+
+    const { mutate: updateViews } = useMutation(
+        async () => {
+            console.log('updating views');
+
+            const postRef = doc(db, 'posts', postId);
+
+            await updateDoc(postRef, { views: increment(1) });
+        },
+        {
+            onSuccess: () => {
+                queryClient.setQueryData(['posts', postId], post => ({
+                    ...post,
+                    views: post.views + 1,
+                }));
+            },
+        }
+    );
+
+    async function likeHandler() {
+        // is author
+        if (userRecord.uid === post.author) return;
+        // hasLiked
+        if (post.likes.includes(userRecord.uid)) {
+            const postRef = doc(db, 'posts', postId);
+
+            await updateDoc(postRef, {
+                likes: post.likes.filter(likerId => likerId !== userRecord.uid),
+            });
+
+            queryClient.setQueryData(['posts', postId], post => ({
+                ...post,
+                likes: post.likes.filter(likerId => likerId !== userRecord.uid),
+            }));
+
+            return;
+        }
+        // hasNotLiked
+        const postRef = doc(db, 'posts', postId);
+
+        if (post.dislikes.includes(userRecord.uid)) {
+            await updateDoc(postRef, {
+                dislikes: post.dislikes.filter(
+                    disliker => disliker !== userRecord.uid
+                ),
+            });
+            queryClient.setQueryData(['posts', postId], post => ({
+                ...post,
+                dislikes: post.dislikes.filter(
+                    disliker => disliker !== userRecord.uid
+                ),
+            }));
+        }
+
+        await updateDoc(postRef, {
+            likes: [...post.likes, userRecord.uid],
+        });
+
+        queryClient.setQueryData(['posts', postId], post => ({
+            ...post,
+            likes: [...post.likes, userRecord.uid],
+        }));
+    }
+
+    async function dislikeHandler() {
+        // is author
+        if (userRecord.uid === post.author) return;
+        // hasDisliked
+        if (post.dislikes.includes(userRecord.uid)) {
+            const postRef = doc(db, 'posts', postId);
+
+            await updateDoc(postRef, {
+                dislikes: post.dislikes.filter(
+                    dislikerId => dislikerId !== userRecord.uid
+                ),
+            });
+            queryClient.setQueryData(['posts', postId], post => ({
+                ...post,
+                dislikes: post.dislikes.filter(
+                    dislikerId => dislikerId !== userRecord.uid
+                ),
+            }));
+            return;
+        }
+        // hasNotDisliked
+        const postRef = doc(db, 'posts', postId);
+
+        console.log('predi da mahne like');
+
+        if (post.likes.includes(userRecord.uid)) {
+            console.log('vytre');
+            console.log(
+                post.likes.filter(likerId => likerId !== userRecord.uid)
+            );
+            await updateDoc(postRef, {
+                likes: post.likes.filter(likerId => likerId !== userRecord.uid),
+            });
+            queryClient.setQueryData(['posts', postId], post => ({
+                ...post,
+                likes: post.likes.filter(likerId => likerId !== userRecord.uid),
+            }));
+        }
+
+        console.log('sled kato mahne like');
+
+        console.log([...post.dislikes, userRecord.uid]);
+
+        await updateDoc(postRef, {
+            dislikes: [...post.dislikes, userRecord.uid],
+        });
+
+        queryClient.setQueryData(['posts', postId], post => ({
+            ...post,
+            dislikes: [...post.dislikes, userRecord.uid],
+        }));
+    }
 
     if (isLoading) {
         return <section id="single-post" className="skeleton"></section>;
@@ -53,26 +193,26 @@ function SinglePostPage() {
                 <section id="content">
                     <div className="header">
                         <div className="author-info">
-                            <div className="profile-pic-cutter">
-                                <img src="./jesse.jpg" alt="profile" />
-                            </div>
-                            <h2 data-title="user">Jesse Pinkman</h2>
+                            <ProfilePicCutter user={author} />
+                            <h2>{author?.username}</h2>
                         </div>
-                        <div className="options">
-                            {userRecord?.role === 'moderator' && (
-                                <>
-                                    <i
-                                        style={{ color: 'crimson' }}
-                                        className="fa-solid fa-lock"
-                                    ></i>
+                        {userRecord?.role === 'moderator' && (
+                            <div className="options">
+                                {post.locked ? (
                                     <i
                                         style={{ color: 'yellowgreen' }}
                                         className="fa-solid fa-lock-open"
                                     ></i>
-                                </>
-                            )}
-                        </div>
+                                ) : (
+                                    <i
+                                        style={{ color: 'crimson' }}
+                                        className="fa-solid fa-lock"
+                                    ></i>
+                                )}
+                            </div>
+                        )}
                     </div>
+
                     <img src={post.url} alt="blog-img" className="image" />
 
                     <div className="stats">
@@ -85,11 +225,25 @@ function SinglePostPage() {
                                 <span>{post.views}</span>
                             </div>
                             <div className="interaction">
-                                <i className="fa-regular fa-thumbs-up"></i>
+                                <i
+                                    onClick={likeHandler}
+                                    className={`fa-${
+                                        post.likes.includes(userRecord.uid)
+                                            ? 'solid'
+                                            : 'regular'
+                                    } fa-thumbs-up`}
+                                ></i>
                                 <span>{post.likes.length}</span>
                             </div>
                             <div className="interaction">
-                                <i className="fa-regular fa-thumbs-down"></i>
+                                <i
+                                    onClick={dislikeHandler}
+                                    className={`fa-${
+                                        post.dislikes.includes(userRecord.uid)
+                                            ? 'solid'
+                                            : 'regular'
+                                    } fa-thumbs-down`}
+                                ></i>
                                 <span>{post.dislikes.length}</span>
                             </div>
 
@@ -122,9 +276,6 @@ function SinglePostPage() {
                                     ></div>
                                 </div>
                             </section>
-                        </div>
-                        <div className="comments">
-                            <h1>Comments</h1>
                         </div>
                     </div>
                 </section>
